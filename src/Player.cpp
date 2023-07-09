1826b19d9ed16aa363e2b6a1dc4c1e40054972cc
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-07-09 18:18:49 -0400

    Fix issues with g++ and libstdc++.
    - std::fabsf is not available https://gcc.gnu.org/bugzilla/show_bug.cgi?id=79700
    - explicitly cast the elapsed time to float.

--- src/Player.cpp
+++ src/Player.cpp
@@ -9,7 +9,8 @@ Player::Player(float x, float y, float width, float height)
 SDL_FRect Player::to_SDL_frect() const { return SDL_FRect{m_x, m_y, m_width, m_height}; }
 
 void Player::update(const std::vector<Obstacle> &obstacles, const std::vector<Command> &commands, Uint64 elapsed_time) {
-  float gravity = 0.1f * elapsed_time;
+  float delta = static_cast<float>(elapsed_time);
+  float gravity = 0.1f * delta;
 
   SDL_FRect hitbox = this->to_SDL_frect();
   hitbox.y += gravity;
@@ -29,7 +30,7 @@ void Player::update(const std::vector<Obstacle> &obstacles, const std::vector<Co
   static constexpr float Y_ACCEL = 69.f;
   static constexpr float X_ACCEL = .5f;
   static constexpr float X_FRICTION = .2f;
-  float friction_accel = SDL_clamp(X_FRICTION, 0, std::fabsf(a_x));
+  float friction_accel = SDL_clamp(X_FRICTION, 0.f, std::fabs(a_x));
   a_x += std::copysignf(friction_accel, -1.f * a_x);
   for (auto command : commands) {
     switch (command) {
@@ -37,13 +38,13 @@ void Player::update(const std::vector<Obstacle> &obstacles, const std::vector<Co
       if (!can_jump) {
         break;
       }
-      a_y -= Y_ACCEL * elapsed_time;
+      a_y -= Y_ACCEL * delta;
       break;
     case Command::CMD_LEFT:
-      a_x -= X_ACCEL * elapsed_time;
+      a_x -= X_ACCEL * delta;
       break;
     case Command::CMD_RIGHT:
-      a_x += X_ACCEL * elapsed_time;
+      a_x += X_ACCEL * delta;
       break;
     default:
       break;
