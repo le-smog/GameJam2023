10bcf81c3ae2473e39d1ae683f5a506c4a2c7834
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-07-09 13:56:34 -0400

    Handle vertical collisions.

--- src/Player.cpp
+++ src/Player.cpp
@@ -7,15 +7,15 @@ Player::Player(float x, float y, float width, float height) : m_x(x), m_y(y), m_
 SDL_FRect Player::to_SDL_frect() const { return SDL_FRect{m_x, m_y, m_width, m_height}; }
 
 void Player::update(const std::vector<Obstacle> &obstacles, Uint64 elapsed_time) {
-  // Apply gravity
-  m_y += 0.01f * elapsed_time;
+  float next_y = m_y + 0.1f * elapsed_time;
 
   SDL_FRect hitbox = this->to_SDL_frect();
+  hitbox.y = next_y;
   for (auto &obstacle : obstacles) {
     SDL_FRect obstacle_hitbox = obstacle.to_SDL_frect();
     if (SDL_HasIntersectionF(&hitbox, &obstacle_hitbox)) {
-      // find side of collision
-      // Block speed on the side of the collision
+      return;
     }
   }
+  m_y = next_y;
 }
