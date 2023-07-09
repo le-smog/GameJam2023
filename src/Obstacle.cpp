00a5578f36a6cc8999d863d1200dab1c3459d34a
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-07-08 22:34:34 -0400

    Fix macOS build.

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -2,4 +2,4 @@
 
 Obstacle::Obstacle(int x, int y, int width, int height) : m_x(x), m_y(y), m_width(width), m_height(height) {}
 
-SDL_Rect Obstacle::to_SDL_rect() { return SDL_Rect(m_x, m_y, m_width, m_height); }
+SDL_Rect Obstacle::to_SDL_rect() { return SDL_Rect{m_x, m_y, m_width, m_height}; }
--- src/Player.cpp
+++ src/Player.cpp
@@ -2,13 +2,6 @@
 
 #include <SDL2/SDL.h>
 
-Player::Player(int x, int y, int width, int height): m_x(x), m_y(y), m_width(width), m_height(height) {}
+Player::Player(int x, int y, int width, int height) : m_x(x), m_y(y), m_width(width), m_height(height) {}
 
-SDL_Rect Player::to_SDL_rect() const {
-    return SDL_Rect (
-        m_x,
-        m_y,
-        m_width,
-        m_height
-    );
-}
+SDL_Rect Player::to_SDL_rect() const { return SDL_Rect{m_x, m_y, m_width, m_height}; }
